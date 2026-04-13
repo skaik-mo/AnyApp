86cecd19ea797b3f060d97c7debdb1a18d7d6152
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2026-04-13 17:50:44 +0300

    initial Commit

---
+++ AnyApp/AnyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyAppApp.swift
+//  AnyApp
+//
+//  Created by Mohammed Skaik on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct AnyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
